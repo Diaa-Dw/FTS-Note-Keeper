90f396f082b0905ad77582085af99c61bea6beeb
Diaa-Dw <oava9944@gmail.com>
2025-03-14 13:09:36 +0200

    create User schema with pre-save hook to hash password before storing in DB

---
+++ src/models/userModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please tell us your name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide your email"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "Please provide a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minliength: 8,
+    select: false,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
