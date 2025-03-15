e99fa76829d63dd11e18f85d7cf24f436e0d7b4f
Diaa-Dw <oava9944@gmail.com>
2025-03-15 17:44:32 +0200

    Restrict modifications to note owner & improve error handling

--- src/controllers/noteController.js
+++ src/controllers/noteController.js
@@ -3,43 +3,73 @@ const catchAsync = require("../utils/catchAsync");
 const responseHandler = require("../utils/responseHandler");
 
 exports.createNote = catchAsync(async (req, res, next) => {
+  const { user } = req;
+
   const createdNote = await Note.create({
     title: req.body.title,
     content: req.body.content,
-    user: req.body.user,
+    user: user._id,
   });
 
   responseHandler(res, 201, createdNote);
 });
 
 exports.getAllNotes = catchAsync(async (req, res, next) => {
-  console.log(req.user);
-  const notes = await Note.find();
+  const { user } = req;
+
+  const notes = await Note.find({
+    user: user._id,
+  });
 
   responseHandler(res, 200, notes);
 });
 
 exports.getNoteById = catchAsync(async (req, res, next) => {
+  const { user } = req;
   const { id } = req.params;
-  const note = await Note.findById(id);
+  const note = await Note.findOne({ _id: id, user: user._id });
+
+  if (!note) {
+    next(new Error(`Note not found or unauthorized`));
+  }
 
   responseHandler(res, 200, note);
 });
 
 exports.updateNoteById = catchAsync(async (req, res, next) => {
+  const { user } = req;
   const { id } = req.params;
-  const updatedNote = await Note.findByIdAndUpdate(id, req.body, {
-    new: true,
-    runValidators: true,
-  });
 
+  const updatedNote = await Note.findOneAndUpdate(
+    {
+      _id: id,
+      user: user._id,
+    },
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  if (!updatedNote) {
+    next(new Error(`Note not found or unauthorized`));
+  }
   responseHandler(res, 200, updatedNote);
 });
 
 exports.deleteNoteById = catchAsync(async (req, res, next) => {
+  const { user } = req;
   const { id } = req.params;
 
-  await Note.findByIdAndDelete(id);
+  const deletedNote = await Note.findOneAndDelete({
+    _id: id,
+    user: user._id,
+  });
+
+  if (!deletedNote) {
+    next(new Error(`Note not found or unauthorized`));
+  }
 
   responseHandler(res, 204, null);
 });
