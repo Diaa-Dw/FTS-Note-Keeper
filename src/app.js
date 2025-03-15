9a2f836282979574fa649f2f8d7517ece12d08a5
Diaa-Dw <oava9944@gmail.com>
2025-03-16 01:36:34 +0200

    implement global error handler and AppError class for enhanced error handling

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const cookiePareser = require("cookie-parser");
 //Routes
 const noteRoutes = require("./routes/noteRoutes");
 const userRoutes = require("./routes/userRoutes");
+const globalErrorHandler = require("./controllers/errorController");
+const AppError = require("./utils/appError");
 
 const app = express();
 
@@ -12,5 +14,12 @@ app.use(cookiePareser());
 
 app.use("/api/v1/notes", noteRoutes);
 app.use("/api/v1/users", userRoutes);
+
+//For handling unimplement routes
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
